0b20ba9dc48be8427483230bd6167402aaadc0ed
Nikosbatz <nikos.mpatz@outlook.com>
2024-01-02 03:23:07 +0200

    NB first classification report

--- Naive Bayes/NB.py
+++ Naive Bayes/NB.py
@@ -10,35 +10,45 @@ class NB:
         self.positiveWordsProbs = {}
         self.probNeg = 0
         self.probPos = 0
+        self.countNeg = 0
+        self.countPos = 0
     
-    def train(self):
+    def train(self) -> None:
         fPos = open(os.getcwd() + "\\Raw data processing\\PositiveFreq.txt", "r")
         fNeg = open(os.getcwd() + "\\Raw data processing\\NegativeFreq.txt", "r")
         
-
-        countPos = int(fPos.readline().split()[1])
-        countNeg = int(fNeg.readline().split()[1])
-        
-        self.probNeg = countNeg / (countNeg + countPos)
-        self.probPos = countPos / (countNeg + countPos)
         
 
+        self.countPos = int(fPos.readline().split()[1])
+        self.countNeg = int(fNeg.readline().split()[1])
+        
+        self.probNeg = self.countNeg / (self.countNeg + self.countPos)
+        self.probPos = self.countPos / (self.countNeg + self.countPos)
+        
+        # Initiating Dictionaries containing calculated word probabilities (So Laplace smoothing can be used).
+        
+        
+        
 
+
+        # Calculating P(Xi|C) using Laplace smoothing
         line = fPos.readline()
         while line != "":
-            self.positiveWordsProbs[line.split()[0]] = int(line.split()[1]) / countPos
+            self.positiveWordsProbs[line.split()[0]] = int(line.split()[1]) +1 / self.countPos +2
             line = fPos.readline()
     
         line = fNeg.readline()
         while line != "":
-            self.negativeWordsProbs[line.split()[0]] = int(line.split()[1]) / countNeg
+            self.negativeWordsProbs[line.split()[0]] = int(line.split()[1]) +1/ self.countNeg +2
             line = fNeg.readline()
 
         fPos.close()
         fNeg.close()
         
 
-    def predict(self, m, n, k):
+    def predict(self, xTest, m, n, k) -> list:
+
+        # creates custom dictionary based on the hyper-parameters (m, n, k) given.
         customDict = []
         OverallFile = open(os.getcwd() + "\\Raw data processing\\OverallFreq.txt", "r")
         lines =  OverallFile.readlines()
@@ -48,37 +58,59 @@ class NB:
         
         OverallFile.close()
 
-        # TESTING FILES
-        file = open(os.getcwd() + "\\Raw data processing\\aclImdb\\test\\neg\\4_4.txt", "r")
+        predictedValues = []
+        for review in xTest:
+            reviewList = review.split()
+            review = {}
+            # Initiate the dictionary containing the vector
+            for i in reviewList:
+                review[i] = 0
+            # Specifying which words the review contains (0 or 1 in the vector attributes)
+            for item in review.keys():
+                review[item] = item in customDict
+            
+            # NEGATIVE
+            negProdResult = 1
+            for item in review.items():
+                if item[1] == 1:
+                    if item[0] in self.negativeWordsProbs:
+                        negProdResult *= self.negativeWordsProbs[item[0]]
+                    else:
+                        negProdResult = 1 / self.countNeg +2
 
-        review = file.read()
-        reviewList = review.split()
-        review = {}
-        # Initiate the dictionary containing the vector
-        for i in reviewList:
-            review[i] = 0
-        # Specifying which words the review contains (0 or 1 in the vector attributes)
-        for item in review.keys():
-            review[item] = item in customDict
+            negProdResult *= self.probNeg
+            
+            # POSITIVE
+            posProdResult = 1
+            for item in review.items():
+                if item[1] == 1:
+                    if item[0] in self.positiveWordsProbs:
+                        posProdResult *= self.positiveWordsProbs[item[0]]
+                    else:
+                        posProdResult = 1 / self.countPos +2
+
+            posProdResult *= self.probPos
+
+            predictedValues.append( 1 if posProdResult>negProdResult else 0)
+            
+        return predictedValues
+            
+
+
+    def loadTestData(self) -> list:
+        data = []
+        expected = []
+        path = os.getcwd() + "\\Raw data processing\\aclImdb\\test\\neg\\"
+        for file in os.listdir(path):
+            f = open(path + file, "r", errors="ignore")
+            data.append(f.read())
+            expected.append(0)
         
-        # NEGATIVE
-        negProdResult = 1
-        for item in review.items():
-            if item[1] == 1:
-                negProdResult *= self.negativeWordsProbs[item[0]]
-                print(negProdResult)
+        path = os.getcwd() + "\\Raw data processing\\aclImdb\\test\\pos\\"
+        for file in os.listdir(path):
+            f = open(path + file, "r", errors="ignore")
+            data.append(f.read())
+            expected.append(1)
 
-        negProdResult *= self.probNeg
+        return [data, expected]
         
-
-        # POSITIVE
-        posProdResult = 1
-        for item in review.items():
-            if item[1] == 1:
-                posProdResult *= self.positiveWordsProbs[item[0]]
-                print(posProdResult)
-
-        posProdResult *= self.probPos
-        
-        print( posProdResult > negProdResult)
-
--- Naive Bayes/app.py
+++ Naive Bayes/app.py
@@ -1,8 +1,15 @@
 from NB import NB
+from sklearn.metrics import classification_report
 
 naiveB = NB()
 naiveB.train()
-naiveB.predict(500, 50, 9000)
+print("-----------------")
+#naiveB.predict(500, 50, 9000)
+xTest, yTest = naiveB.loadTestData()
+predicted = naiveB.predict(xTest=xTest, m=9000, n=50, k=9000)
 
+print("-----------------")
 
+print(classification_report(yTest, predicted,
+                            zero_division=1))
 
