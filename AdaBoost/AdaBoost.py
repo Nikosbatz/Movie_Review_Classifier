330bdae1e8272f2556428acd036ced421400f203
Nikosbatz <nikos.mpatz@outlook.com>
2024-01-07 16:39:28 +0200

    id3 update

--- AdaBoost/AdaBoost.py
+++ AdaBoost/AdaBoost.py
@@ -1,15 +1,27 @@
-
+# importing custom functions.
+#--------
+import sys
+sys.path.append(os.getcwd() + "\\Raw data processing")
+from loadData import *
+from createVocabulary import *
+#--------
 
 
 class AdaBoost:
 
-    def __init__(self, m, n, k):
+    def __init__(self, estimators, m, n, k):
         self.m = m
         self.n = n
         self.k = k
+        self.estimators = estimators
 
-    def fit(self):
-        None
+    def fit(self, xTrain):
+        
+        vocabulary = createVocabulary(self.m, self.n, self.k)
+        xVector = createVector(xTrain, vocabulary)
+        
+        xTrainLen = len(xTrain)
+        weights = ([1]*xTrainLen) / xTrainLen
 
     def predict(self):
         None
