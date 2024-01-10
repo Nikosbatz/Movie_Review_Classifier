dc4fad75ccb1cd529eb7343fece41f4fb6eb54b6
0D-Coder <148544020+0D-Coder@users.noreply.github.com>
2024-01-10 12:36:01 +0200

    Update RandomForest.py

--- RandomForest/RandomForest.py
+++ RandomForest/RandomForest.py
@@ -1,33 +1,50 @@
-from AI_Project_2 import DecisionTree
+from sklearn.utils import resample
+from sklearn.metrics import accuracy_score
+from id3 import ID3
 import numpy as np
-from collections import Counter
+import sys
+import os
+sys.path.append(os.getcwd() + "\\Raw data processing")
+from createVocabulary import *
+from loadData import *
+
 
 class RandomForest:
-    def __init__(self, n_trees = 10, max_depth = 10, min_samples_split = 2, n_features = None):
-        self.n_trees = n_trees
-        self.max_depth = max_depth
-        self.min_samples_split = min_samples_split
-        self.n_features = n_features
+    def __init__(self, n_estimators, features, m, n, k):
+        self.n_estimators = n_estimators
+        self.features = features
+        self.m = m
+        self.n = n
+        self.k = k
         self.trees = []
 
-    def fit(self, X, Y):
-        self.trees = []
-        for _ in range(self.n_trees):
-            tree = DecisionTree(max_depth = self.max_depth, min_samples_split=self.min_samples_split, n_features=self.n_features)
-            X_sample, Y_sample = self.bootstrap_samples(X, Y)
-            tree.fit(X_sample, Y_sample)
+    def fit(self, x, y):
+        for _ in range(self.n_estimators):
+            # Bootstrap sampling - random sampling with replacement
+            x_sample, y_sample = resample(x, y, replace=True, random_state=np.random.randint(1000))
+            
+            # Create and train a decision tree using ID3
+            tree = ID3(self.features,self.m , self.n, self.k)
+            tree.fit(x_sample, y_sample)
+            
+            # Add the trained tree to the forest
             self.trees.append(tree)
 
-    def bootstrap_samples(self, X, Y):
-        n_samples = X.shape[0]
-        idxs = np.random.choice(n_samples, n_samples, replace=True)
-        return X[idxs], Y[idxs]
-    
-    def most_common_label(self, Y):
-        counter = Counter(Y)
-        value = counter.most_common(1)[0][0]
-        return value
+    def predict(self, x):
+        # Make predictions using each tree and combine them through majority voting
+        predictions = np.array([tree.predict(x) for tree in self.trees])
+        ensemble_predictions = np.apply_along_axis(lambda x: np.bincount(x).argmax(), axis=0, arr=predictions)
+        return ensemble_predictions
 
-    def predict(self, X):
-        predictions = np.array([tree.predict(X) for tree in self.trees])
-        tree_preds = np.swapaxes(predictions, 0, 1)
+# Example usage:
+vocabulary = createVocabulary(100, 10, 1000)
+xTrain, yTrain = loadTrainData()
+
+RF = RandomForest(5, vocabulary, 100, 10, 1000)
+RF.fit(xTrain, yTrain)
+RF.predict(xTrain)
+
+from sklearn.metrics import accuracy_score
+
+accuracy = accuracy_score(yTrain, RF.predict(xTrain))
+print(accuracy)
